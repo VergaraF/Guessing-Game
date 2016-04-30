ea8e57bf35751b040465ed796f674cc8a0051cbe
Fabian Vergara <vergarf@gmail.com>
2016-04-30 09:56:13 -0400

    Finished app

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -15,8 +15,22 @@ class ViewController: UIViewController {
     
     
     @IBAction func guessButton(sender: AnyObject) {
+        let randomNum:Int = Int(arc4random_uniform(6))
+        
+        if (Int(numberGivenByUser.text!)! == randomNum){
+            attemptResultLabel.textColor = UIColor.greenColor()
+            attemptResultLabel.text = "Correct! The number was \(randomNum)"
+        }else{
+            attemptResultLabel.textColor = UIColor.redColor()
+            attemptResultLabel.text = "Sorry! The number was \(randomNum)"
+            
+        }
+        
+        attemptResultLabel.hidden = false;
         
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
